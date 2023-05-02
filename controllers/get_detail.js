8ae394644306e34a6f26b5fbd60a234782c3290d
Gustavo Engers <gustavoengers6441@gmail.com>
2023-05-02 19:13:23 -0300

    Scraping para obtener el texto de los párrafos de la página

---
+++ controllers/get_detail.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+/**
+ * Genera los objetos con los datos necesarios seleccionados del DOM obtenido de la solicitud
+ * @param data HTML obtenido de la solicitud
+ * @returns Array con los datos generados
+ */
+function generateResults(data, language = 'es') {
+  const URL = `https://${language}.wikipedia.org`;
+  const $ = cheerio.load(data);
+  const obj = {
+    title: $('#firstHeading').text(),
+    text: '',
+  };
+
+  $('p')
+    .nextAll()
+    .each(function (i, el) {
+      obj.text =
+        obj.text +
+        $(this)
+          .text()
+          .replace(/\[\d+\]/g, '')
+          .replace(/\d+\.\d+/g, '');
+    });
+  return obj;
+}
+
+/**
+ * Obtiene el texto de los párrafos de la página
+ * @param url Dirección url de la información detallada
+ * @returns Objeto con el título y los párrafos
+ */
+async function getDetail(url) {
+  if (!url) throw new Error('Error: Missing parameter!');
+  const response = await axios
+    .get(url)
+    .then((d) => generateResults(d.data))
+    .catch((e) => e);
+  return response;
+}
+
+module.exports = getDetail;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const getLanguages = require('../controllers/get_languages');
 const getSearch = require('../controllers/get_search');
+const getDetail = require('../controllers/get_detail');
 
-router.get('/language', async (req, res) => {
+router.get('/languages', async (req, res) => {
   try {
     let data = await getLanguages();
     res.json(data);
@@ -16,6 +17,16 @@ router.get('/search', async (req, res) => {
   const { language, query } = req.body;
   try {
     let data = await getSearch(language, query);
+    res.json(data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.get('/detail', async (req, res) => {
+  const { url } = req.body;
+  try {
+    let data = await getDetail(url);
     res.json(data);
   } catch (error) {
     res.status(500).send(error.message);
