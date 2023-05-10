2c1cf90a81694175bfdee6d25ad5d096ce6edbcf
Gustavo Engers <gustavoengers6441@gmail.com>
2023-05-10 10:43:10 -0300

    Agregando el maldito CORS

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = require('./routes');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE, PATCH'
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use('/', router);
